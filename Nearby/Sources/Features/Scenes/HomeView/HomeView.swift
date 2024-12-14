f9cde403028d8ff4afa2cd8d91d5dff02dd14864
MarcosVel <marcosvelosodev@gmail.com>
2024-12-14 15:39:49 -0300

    feat: item cell component

---
+++ Nearby/Sources/Features/Scenes/HomeView/HomeView.swift
@@ -0,0 +1,8 @@
+//
+//  HomeView.swift
+//  Nearby
+//
+//  Created by Marcos Roberto Lemos Veloso on 11/12/24.
+//
+
+import Foundation
---
+++ Nearby/Sources/Features/Scenes/HomeView/HomeViewController.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewController.swift
+//  Nearby
+//
+//  Created by Marcos Roberto Lemos Veloso on 11/12/24.
+//
+
+import Foundation
